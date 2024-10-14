bcdb31aea882de587d08d7d4b58d370bacb4b036
Saulo Gomes <saulogcarmo@gmail.com>
2024-10-14 20:16:32 -0300

    app

---
+++ app.js
@@ -0,0 +1,20 @@
+/**
+ * PWA - Flex
+ * @author Professor José de Assis
+ * @link https://joseassis.com.br/
+ */
+
+function calcular() {
+    // pegar valores dos inputs
+    let gasolina = Number(document.getElementById('gasolina').value)
+    let etanol = Number(document.getElementById('etanol').value)
+    let kmlGasolina = Number(document.getElementById('kmGasolina').value)
+    let kmlEtanol = Number(document.getElementById('kmEtanol').value)
+    // calcular a vantagem
+    if (etanol < (kmlEtanol / kmlGasolina) * gasolina) {
+        document.getElementById("status").src = "./img/etanol.png"
+    } else {
+        document.getElementById("status").src = "./img/gasolina.png"
+    }
+
+}
